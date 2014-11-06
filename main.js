b07cecb37aef8a568c126e75b6cfd3ee94f0e72c
Hal Rogers <hal.c.rogers@gmail.com>
2014-11-05 20:18:15 -0500

    tweeting functionality works!

--- main.js
+++ main.js
@@ -1,2 +1,46 @@
 var util = require('util');
-var twitter = require('twitter');
+var Twitter = require('twitter');
+
+var secrets = require('./config/secrets.json');
+var twit = new Twitter(secrets);
+
+var theGreatGatsby = readTheGreatGatsby("res/the_great_gatsby.txt");
+
+twit.verifyCredentials(postIfCredentialsAreValid);
+
+function postIfCredentialsAreValid(data) {
+  var functionName = "validation";
+  if (logSuccessOrFailure(data, functionName)) {
+    functionName = "tweet";
+    tweet("");
+    twit.updateStatus("hello world", function(data) {
+      if (logSuccessOrFailure(data)) {
+        console.log()
+      }
+    });
+  }
+}
+
+function tweet(status) {
+  
+}
+
+function logSuccessOrFailure(data, functionName) {
+  if (typeof data["statusCode"] == "undefined" || data["statusCode"] == 200) {
+    console.log(functionName + " succeeded");
+    return true;
+  } else {
+    console.log(util.inspect(data));
+    console.log(functionName + " failed");
+    return false;
+  }
+}
+
+function readFile(filepath) {
+  var options = {
+    flags: 'r',
+    encoding: 'utf8'
+  }
+  var file = fs.readFileSync(path, options);
+  return file;
+}
